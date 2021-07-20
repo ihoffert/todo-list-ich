90f5e29df48a2ee93a3e8786a3799afc9bfc1417
ingrid hoffert <ingihoff@gmail.com>
2021-07-20 11:00:52 -0700

    Initial Commit

---
+++ todo-list-ich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  todo-list-ich
+//
+//  Created by Ingrid Hoffert on 7/20/21.
+//  Copyright © 2021 Ingrid Hoffert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ todo-list-ichTests/todo_list_ichTests.swift
@@ -0,0 +1,34 @@
+//
+//  todo_list_ichTests.swift
+//  todo-list-ichTests
+//
+//  Created by Ingrid Hoffert on 7/20/21.
+//  Copyright © 2021 Ingrid Hoffert. All rights reserved.
+//
+
+import XCTest
+@testable import todo_list_ich
+
+class todo_list_ichTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
